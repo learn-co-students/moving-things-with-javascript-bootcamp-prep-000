33981f257fa16698844b27f5ba428c7002ff23e4
Cindy Gao <cindygao@hotmail.com>
2016-09-04 14:45:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const app = "I don't do much."
+
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.left.replace('px', '')
+  var right = parseInt(leftNumbers, 10)
+
+  if (right > 0) {
+    dodger.style.right = `${right + 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 39) {
+    moveDodgerRight()
+  }
+})
