7bda8c611d0273d1756c67c7bf75f9f47305079e
Cindy Gao <cindygao@hotmail.com>
2016-09-04 14:54:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 
-document.ElementById("dodger").style.right = "180px"
+document.getElementById("dodger").style.right = "180px"
 
 function moveDodgerRight() {
   var rightNumbers = dodger.style.left.replace('px', '')
