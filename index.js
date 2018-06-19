01d314ec5dc6a5083a645404315878c108fa67f4
brianhkang1@gmail.com <brianhkang1@gmail.com>
2018-06-19 21:37:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const app = "I don't do much."
+
+var dodger = document.getElementById('dodger')
+
+    function moveDodgerRight() {
+      var rightNumbers = dodger.style.left.replace('px', '')
+      var right = parseInt(rightNumbers, 10)
+ 
+       if (right < 360) {
+         dodger.style.left = `${right + 1}px`
+        }
+      }
+
+    document.addEventListener('keydown', function(e) {
+      if (e.which === 37) {
+        moveDodgerLeft()
+      }
+    })
