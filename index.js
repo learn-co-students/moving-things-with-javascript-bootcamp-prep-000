783d95f593cf7076065bcc55154ac4873f806a8f
Rodolfo Guzman <rodolfoguzman147@gmail.com>
2018-06-20 12:43:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-const app = "I don't do much."
+var dodger=document.getElementByID('dodger')
+dodger.style.backgroundColor= '#FF89302'
+var leftNumbers= dodger.style.left.replace('px','')
+var left=parseInt(leftNumbers,10)
+
+function moveDodgerLeft(){
+    if(left>0){
+      dodger.style.left=`${left-1}px`
+    }
+}
+
+function moveDodgerRight(){
+  if(left<360){
+    dodger.style.left=`${left+1}px`
+  }
+}
+
+document.addEventListener('keydown',function(e){ if (e.which==37){
+    moveDodgerLeft()
+  } 
+  if (e.which==39){
+    moveDodgerRight()
+  }
+})
