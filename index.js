acea427f08b78031b4b691b13b54acd50ca47a3d
Diana Yoo <diana.yooj@gmail.com>
2018-06-21 00:18:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const app = "I don't do much."
+
+var dodgers = document.getElementById('dodgers');
+
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', '');
+  var right = parseInt(rightNumbers, 10);
+        
+  if (right>0) {
+    dodger.style.right = `${right-0}px`;
+  }
+}
+      
+document.addEventListener('keydown', function(e) {
+  if (e.which === 39) {
+    moveDodgerRight();
+  }
+});
+      
