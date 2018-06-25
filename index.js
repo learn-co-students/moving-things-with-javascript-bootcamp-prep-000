54b4efc1d4dac7ed1801bb5cf11526b4e6560d7e
raina <rainaoo2017@gmail.com>
2018-06-25 12:30:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const app = "I don't do much."
+
+var dodger = document.getElementById('dodger')
+
+function moveDodgerRight() {
+  var RightNumbers = dodger.style.left.replace('px', '')
+  var Right = parseInt(RightNumbers, 10)
+ 
+  if (Right > 0) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerRight()
+  }
+})
