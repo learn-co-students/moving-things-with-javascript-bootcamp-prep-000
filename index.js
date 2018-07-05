e9da546b37227e94230c513282d829368120dcec
scalande@yahoo.com <scalande@yahoo.com>
2018-07-05 15:24:53 +0000

    Done.

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ function moveDodgerRight(){
       var leftNumbers = dodger.style.left.replace('px', '')
     var left = parseInt(leftNumbers, 10)
  
+   if (left > 0) {
     dodger.style.left = `${left +1}px`
+}
 }
 
 document.addEventListener('keydown', function(e) {
