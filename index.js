283530c18a3ee967c82bbe01ce9901a80979b146
scalande@yahoo.com <scalande@yahoo.com>
2018-07-05 15:20:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const app = "I don't do much."
+var dodger = document.getElementById('dodger');
+
+function moveDodgerRight(){
+      var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+ 
+    dodger.style.left = `${left +1}px`
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerRight()
+}
+
+})
+
