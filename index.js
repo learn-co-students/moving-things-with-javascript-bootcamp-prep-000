e4ce057e156aa773ca35c5320dd3dce6e41f4f61
christleyjordan@gmail.com Christley <christleyjordan@gmail.com>
2018-07-10 02:51:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const app = "I don't do much."
+var dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+function moveDodgerRight() {
+  var rightNumbers = parseInt(dodger.style.left.replace('px', ''));
+  rightNumbers += parseInt(dodger.style.width.replace('px', ''), 10);
+ 
+  if (right < dodger.parentNode.style.width) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
