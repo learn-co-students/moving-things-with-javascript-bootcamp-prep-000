95285f59e1f9673b07c88ccca48f0cd89528394b
Abrahym Eldden <asharfeldden@gmail.com>
2018-07-11 11:27:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const app = "I don't do much."
+var dodge = document.getElementById('dodger');
+function moveDodgerLeft(){
+  var leftNumbers = dodge.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+  dodger.style.left = `${left - 1}px`
+}
+      
+function moveDodgerRight(){
+  var rightNumbers = dodge.style.left.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+  dodger.style.left = `${right + 1}px`
+}
+
+document.addEventListener('keydown', function(e) {
+  if(e.which === 37) {
+    moveDodgerLeft();
+  }
+  if(e.which === 39){
+    moveDodgerRight();
+  }
+});
