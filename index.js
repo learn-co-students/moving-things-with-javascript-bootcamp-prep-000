b6642ec90afdd677b24c2918fe21da0fc5f237b3
Sabrie Morales <sabriemorales1995@gmail.com>
2018-07-14 18:53:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const app = "I don't do much."
+
+
+var dodger= document.getElementById('dodger');
+
+
+function moveDodgerLeft(){
+  var leftNum = dodger.style.left.replace('px','');
+  var left = parseInt(leftNum,10);
+  
+  if(left>0){
+    dodger.style.left = `${left-1}px`;
+  }
+}
+
+
+function moveDodgerRight(){
+  var rightNum = dodger.style.right.replace('px','');
+  var right = parseInt(rightNum,10);
+}
