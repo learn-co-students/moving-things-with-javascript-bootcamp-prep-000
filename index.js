f9d3acf1969b44282e6537c9ff9d0d994c41b50a
shofi384@gmail.com Rahman <shofi384@gmail.com>
2018-07-17 19:30:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const app = "I don't do much."
+
+var dodger = document.getElementById('dodger')
+
+dodger.style.backgroundColor = '#FF69B4'
+
+document.addEventListener('keydown', function(e) {
+  console.log(e.which)
+})
+
+document.addEventListener('keydown', function(e) {
+  if(e.which === 37) {
+      var leftText = dodger.style.left.replace('px', '')
+      var left = parseInt(leftText)
+      if(left > 0) {
+          dodger.style.left = `${left - 1}px`
+      }
+  }
+})
