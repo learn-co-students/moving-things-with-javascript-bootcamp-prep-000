9336f23d279c47867a9b000c643779be15faeeb4
bnreine <41244421+bnreine@users.noreply.github.com>
2018-07-24 16:53:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-const app = "I don't do much."
+var dodger = document.getElementById('dodger');
+var box = document.getElementById('game');
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+  var dodgerWidthNumbers = $('#dodger').css('width').replace('px','')
+  var dodgerWidth = parseInt(dodgerWidthNumbers, 10)
+  var boxWidthNumbers = $('#game').css('width').replace('px','')
+  var boxWidth = parseInt(boxWidthNumbers, 10)
+  
+
+  if (left < (boxWidth-dodgerWidth)) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 39) {
+    moveDodgerRight()
+  }
+})
