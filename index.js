99fdf8611b406facc771b7bb0819e650b153f25a
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-07-26 23:42:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+var dodger = document.getElementById(`dodger`)
+      dodger.style.backgroundColor=`#FF69B4`
+      
+      function moveDodgerLeft(){
+        
+        var leftNumbers = dodger.style.left.replace(`px`,``)
+        var left = parseInt(leftNumbers,10)
+        if(left>0){
+          dodger.style.left=`${left-1}px`
+        }
+      }
+      
+       function moveDodgerRight(){
+        
+        var rightNumbers = dodger.style.left.replace(`px`,``)
+        var right = parseInt(rightNumbers,10)
+        if(right>0){
+          dodger.style.left=`${right+1}px`
+        }
+      }
+      
+ 
+       document.addEventListener(`keydown`,function(f){
+
+        if(f.which ===39){
+          moveDodgerRight()
+        }
+        if(f.which===37){
+          moveDodgerLeft()
+        }
+        
+      })
+      
+      
+      
+      
