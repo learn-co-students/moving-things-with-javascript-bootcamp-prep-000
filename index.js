8d36053e6ed53095c68b4fecaa2b59bc140a1095
Charles Kim <cwkim3@gmail.com>
2018-08-10 23:50:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-const app = "I don't do much."
+function moveDodgerRight(){
+	var leftNumber = dodger.style.left.replace('px',' ');
+    var left = parseInt(leftNumber, 10);
+    if (left < 360){
+        dodger.style.left = `${left + 1}px`}
+}
