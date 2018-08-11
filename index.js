2551968ed92fd5fcf6cdbadb84fbeff117d650df
Guy Bryant <guy.s.bryant@gmail.com>
2018-08-11 21:47:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-const app = "I don't do much."
+$(function(){
+  var dodger = document.getElementById('dodger');
+  
+  document.addEventListener('keydown', function(e){
+    if (e.key === 'ArrowLeft'){
+      moveDodgerLeft();
+    }
+    if (e.key === 'ArrowRight'){
+      moveDodgerRight();
+    }
+  })
+  
+  function moveDodgerLeft(){
+    var currentPosition = parseInt(dodger.style.left);
+    if (currentPosition > 0){
+      dodger.style.left = `${currentPosition - 10}px`;
+    }
+  }
+  
+  function moveDodgerRight(){
+    var currentPosition = parseInt(dodger.style.left);
+    if (currentPosition < 360){
+      dodger.style.left = `${currentPosition + 10}px`;
+    }
+  }
+})
