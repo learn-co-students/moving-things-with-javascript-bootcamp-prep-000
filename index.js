d518310d695a122952e1f8f0fbad4c10f56b0bcc
noah.stewart9813@gmail.com <noah.stewart9813@gmail.com>
2018-08-13 20:10:11 +0000

    Done.

--- index.js
+++ index.js
@@ -8,22 +8,18 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  var rightNumbers = dodger.style.right.replace('px', '')
-  var right = parseInt(rightNumbers, 10)
-  
-  if (right < 360) {
-    dodger.style.right = `${right + 1}px`
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`
   }
 }
 
 document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
     moveDodgerLeft()
-  }
-})
-
-document.addEventListener('keydown', function(e) {
-  if (e.which === 39) {
+  } else if (e.which === 39) {
     moveDodgerRight()
   }
 })
