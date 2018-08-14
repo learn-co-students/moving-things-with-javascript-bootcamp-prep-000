9fb7a5e1fc1ccb49bf9043abc8ebf70fa0bc2a37
Fadi hajj <fadi.elhajj@lau.edu>
2018-08-14 20:36:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+document.addEventListener('keydown', function(e){
+  
+  if(e.which === 37){
+   moveDodger('left'); 
+  }
+  else if(e.which ===39){
+    moveDodger('right');
+  }
+  
+});
+
+function moveDodger(direction){
+  
+  var dodger =  document.getElementById('dodger');
+  var rightNumbers = dodger.style.right.replace('px', '');
+  var right = parseInt(rightNumbers, 10);
+
+  if(right < 360 && direction === 'left')
+    {
+      dodger.style.right = `${right +4}px`;   
+    }
+      
+  else if(right > 0 && direction === 'right')
+    {
+      dodger.style.right = `${right -4}px`; console.log(right);
+    }
+}
+
+  
+
