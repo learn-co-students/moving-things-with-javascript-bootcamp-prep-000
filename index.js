9c3480c9901cd279b02c628d51c848d675f788da
Pearl Zeng <zhuzeng0418@gmail.com>
2018-08-16 21:19:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const app = "I don't do much."
+
+var dodger = document.getElementById('dodger')
+
+dodger.style.backgroundColor = '#FF69B4'
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace('px', '')
+  let left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft()
+  }
+})
+
