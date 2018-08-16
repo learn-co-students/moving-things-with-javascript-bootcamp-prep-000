7a40d52bbbe6fbb4942f84dac6ad970fa6cb6648
Alex S <alex.sunga@fliptech.co>
2018-08-16 07:22:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const app = "I don't do much."
+
+function moveDodgerRight(){
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+  
+  if (right < 0){
+    dodger.style.left = `${right-1}px`
+  
+  }
+}
+
+
