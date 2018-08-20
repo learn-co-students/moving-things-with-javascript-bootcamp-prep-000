3a56ddbf13a0cfb86000eef0ac9bcf0dd5b78ba8
Kaveh Saleminejad <lcaveh@gmail.com>
2018-08-20 23:37:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.left.replace('px', '');
+  var right = parseInt(rightNumbers, 10);
+ 
+  if (right < 400) {
+    dodger.style.left = `${right + 1}px`;
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft();
+  }
+  if (e.which === 39) {
+    moveDodgerRight();
+  }
+});
