26403650c7914df5b2927016b2ed7af601c585e4
Jean-Patrice <jpjocegilbert@gmail.com>
2018-08-23 15:22:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,1 @@
 const app = "I don't do much."
-
-/*var dodger = document.getElementById('dodger')
-dodger.style.backgroundColor = '#FF69B4'
-
-function moveDodgerRight() {
-  var leftNumbers = dodger.style.left.replace('px', '')
-  var left = parseInt(leftNumbers, 10)
-
-  if (dodger.style.left < '360px') {
-    dodger.style.left = `${left + 1}px`
-  }
-}
-
-document.addEventListener('keydown', function(e) {
-  if (e.which === 39) {
-    moveDodgerRight()
-  }
-})
