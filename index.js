7a2032cef2f4efa1595f1caf73f8fdd77229fdcd
Sean Lucius <seanlucius22@icloud.com>
2018-09-01 07:10:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-const app = "I don't do much."
+$(document).ready(function() {
+  
+
+let dodger = document.getElementById('dodger');
+
+function moveDodgerRight() {
+  
+  let rightNumbers = dodger.style.right.replace('px', '');
+  
+  let right = parseInt(rightNumbers, 10);
+  
+  if (right > 0) {
+    
+    dodger.style.right = `${right - 1}px`;
+  }
+  
+}
+
+document.addEventListener('keydown', function(e) {
+  
+  if (e.which === 39) {
+    moveDodgerRight();
+  }
+})
+
+})
