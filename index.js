8e357b6ff2a5389442aa192ac95d6e02d7d29d7b
viqi zevic <vicky.tanzil@gmail.com>
2018-09-05 20:23:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,2 @@
 const app = "I don't do much."
 
-var dodger = document.getElementById('dodger');
-
-function moveDodgerLeft() {
-  var leftNumbers = dodger.style.left.replace('px', '')
-  var left = parseInt(leftNumbers, 10)
- 
-  if (left > 0) {
-    dodger.style.left = `${left - 1}px`
-  }
-}
-
-document.addEventListener('keydown', function(e) {
-  if (e.which === 37) {
-    moveDodgerLeft()
-  }
-})
