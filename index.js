d424ed471efa4e4ba464e9c0a47605eadf472e09
Suheyl Turac <skbsturac@live.com>
2018-09-09 01:02:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const app = "I don't do much."
+function moveDodgerLeft() {
+   var leftNumbers = dodger.style.left.replace('px', '')
+   var left = parseInt(leftNumbers, 10)
+
+   if (left > 0) {
+     dodger.style.left = `${left - 1}px`
+   }
+ }
+
+ function moveDodgerRight() {
+   var rightNumbers = dodger.style.right.replace('px', '')
+   var right = parseInt(rightNumbers, 10)
+
+   if (right > 0) {
+     dodger.style.right = `${right - 1}px`
+   }
+ }
+
+ document.addEventListener('keydown', function(e) {
+   if (e.which === 37) {
+     moveDodgerLeft()
+   }
+   if (e.which === 39) {
+     moveDodgerRight()
+   }
+ }
+ )
