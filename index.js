c6ea63bab29e7fc68b72194610fde5bcf0e517e6
Omar A <oazzat19@gmail.com>
2018-09-11 19:22:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const app = "I don't do much."
+
+/*
+function moveDodgerRight(){
+  var rightNumbers = dodger.style.right.replace("px","")
+  var right = parseInt(rightNumbers,10)
+  
+  if (right<360){
+    dodger.style.right = `${right + 1}px`
+  }
+}
+
+document.addEventListener('keydown',function(key){
+  if(key.which === 39){
+    moveDodgerRight()
+  }
+})
+*/
