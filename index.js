b4ffbee4a190eeed8a3db612c4d7ebf883e6c0ed
Jae Park <wjp241@nyu.edu>
2018-09-11 00:20:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const app = "I don't do much."
+function moveDodgerRight(){
+  var dodger = document.getElementById('dodger');
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`
+  }
+  document.addEventListener('keydown', function(e) {
+  if (e.which === 39) {
+    moveDodgerRight()
+  }
+})
+}
