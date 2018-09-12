3daec75d523307628351044dfbbaab0fdd9a4674
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-12 22:55:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-const app = "I don't do much."
+ var dodger = document.getElementById('dodger');
+      dodger.style.backgroundColor = "#FF69B4";
+      dodger.style.left;
+      dodger.style.right = '180px';
+      dodger.style.bottom = '0px';
+      
+      function moveDodgerLeft() {
+        var leftNumbers = dodger.style.left.replace('px', '');
+        var left = parseInt(leftNumbers, 10);
+        if (left > 0) {
+          dodger.style.left = `${left -1}px`;
+        }
+      }
+      
+      function moveDodgerRight() {
+        var rightNumbers = dodger.style.right.replace('px', '');
+        var right = parseInt(rightnumbers, 10);
+        if (right > 0) {
+          dodger.style.right = `${right -1}px`;
+        }
+      }
+      
+      document.addEventListener('keydown', (e) => {
+        if (e.which === 37) {
+          moveDodgerLeft();
+        }
+        if (e.which === 39) {
+          moveDodgerRight();
+        }
+      });
