4a245b779ea64ddbb42b41cde302b183154b690e
Ben Kudler <bkudler@gmail.com>
2018-09-14 13:51:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const app = "I don't do much."
+
+
+function moveRockRight(){
+  var dodger = document.getElementById('dodger')
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var right = parseInt(leftNumbers, 10);
+  console.log(right)
+  if(right < 360){
+    dodger.style.left = `${right + 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e){
+  if(e.which === 39){
+    moveRockRight();
+  }
+})
