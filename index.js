4cf916c84e848dcb978751a69c53dcefab8cb0f4
Jakob Baumgartner <jakob.baumgartner@hotmail.com>
2018-09-15 18:18:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const app = "I don't do much."
+
+function moveDodgerLeft () {
+  var dodgerNumber = dodger.style.left.replace('px','');
+  console.log(`dodgerNumber is ${dodgerNumber}`);
+
+  var number = parseInt(dodgerNumber, 10);
+
+  if(number>0) dodger.style.left = `${number-1}px`;
+}
+
+function moveDodgerRight () {
+  var dodgerNumber = dodger.style.left.replace('px','');
+  console.log(`dodgerNumber is ${dodgerNumber}`);
+
+  var number = parseInt(dodgerNumber, 10);
+
+  if(number<360) dodger.style.left = `${number+1}px`;
+}
+
+
+$(document).on('keydown', function(keypress) {
+  console.log(keypress.which);
+
+  if(keypress.which == 37) moveDodgerLeft();
+  if(keypress.which == 39) moveDodgerRight();
+
+})
