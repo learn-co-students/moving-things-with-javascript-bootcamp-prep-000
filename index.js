9a639e2886a339ddb98b19b4ac197776901938c1
Joshua Gelberger <joshua_gelberger@alumni.brown.edu>
2018-09-18 03:05:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much."
+
+var dodger = document.getElementById('dodger');
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft();
+  } else if (e.which === 39) {
+    moveDodgerRight();
+  }
+})
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 10}px`
+  }
+}
+
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.left.replace('px', '');
+  var right = parseInt(rightNumbers, 10);
+  
+  if (right < 360) {
+    dodger.style.left = `${right + 10}px`;
+  }
+}
