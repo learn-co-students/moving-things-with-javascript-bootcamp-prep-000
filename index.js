ab837a3357d83e1c71a04bbf14d94b65aebd20f1
Nikhil Gangrade <nikhil12gangrade93@yahoo.com>
2018-09-18 03:43:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const app = "I don't do much."
+function moveDodgerRight(){
+  var rightNumbers = dodger.style.right.replace('px','')
+  var right = parseInt(rightNumbers, 10)
+  if (right > 0){
+    dodger.style.right = `${left + 1}px`
+  }
+  
+}
+
+document.addEventListner('keydown', function(e){
+  if (e.which === 39){
+    moveDodgerRight()
+  }
+});
