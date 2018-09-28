d0218cbb27fc61c353036d3478f7c23403fdb4aa
Skylar Salerno <skylar.salerno@gmail.com>
2018-09-28 00:29:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const app = "I don't do much."
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+ 
+    dodger.style.left = `${left - 1}px`
+  }
+  if(e.which===38){var rightNumbers = dodger.style.right.replace('px','')
+    var right = parseInt(rightNumbers, 10);}
+});
