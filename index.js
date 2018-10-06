872cce84fc76ce95fd34fcb6f21cb865947261c5
Matthew Sherman <msherman0801@gmail.com>
2018-10-06 17:47:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const app = "I don't do much."
+
+
+function moveDodgerRight(e) {
+  if (e.which == 37) {
+    var rightNumbers = dodger.style.right.replace('px', '')
+    var right = parseInt(rightNumbers, 10)
+    
+    if(right > 0) {
+    dodger.style.right = `${right - 1}px`
+    }
+  }
+}
