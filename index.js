d431a031fa858c6e54c1c492879eb39195f7e3ff
severus889700 <severus889700@gmail.com>
2018-10-10 07:01:08 +0000

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ function moveDodgerRight(){
     dodger.style.right = `${right -1}px`
   }
 }
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 39) {
+    moveDodgerRight()
+  }
+})
