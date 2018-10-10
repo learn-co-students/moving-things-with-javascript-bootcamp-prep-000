590aa82c03279c8e987eae4a52a78d5e1dccd654
severus889700 <severus889700@gmail.com>
2018-10-10 06:34:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const app = "I don't do much."
+var dodger = document.getElementById('dodger')
+
+function moveDodgerRight(){
+  var rightNumber = dodger.style.right.replace('px', '');
+  var right = parseInt(rightNumber, 10);
+  
+  if (right>0){
+    dodger.style.right = `${right -1}px`
+  }
+}
