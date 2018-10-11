a85ecc38f65c6a14761454aeb0b2d304b298c23c
Jeff Carter <jeff@pepperfly.com>
2018-10-11 17:58:52 +0000

    Done.

--- index.js
+++ index.js
@@ -7,8 +7,17 @@ document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
     moveDodgerLeft();
   }
+  
+  if (e.which === 38) {
+    moveDodgerUp();
+  }
+  
   if (e.which === 39) {
     moveDodgerRight();
+  }
+  
+  if (e.which === 40) {
+    moveDodgerDown();
   }
 });
 
@@ -25,5 +34,21 @@ function moveDodgerRight() {
   var left = parseInt(leftNumbers, 10);
   if (left < 360) {
     dodger.style.left = `${left + 1}px`;
+  }
+}
+
+function moveDodgerUp() {
+  var bottomNumbers = dodger.style.bottom.replace('px', '');
+  var bottom = parseInt(bottomNumbers, 10);
+  if (bottom < 380) {
+    dodger.style.bottom = `${bottom + 1}px`;
+  }
+}
+
+function moveDodgerDown() {
+  var bottomNumbers = dodger.style.bottom.replace('px', '');
+  var bottom = parseInt(bottomNumbers, 10);
+  if (bottom > 0) {
+    dodger.style.bottom = `${bottom - 1}px`;
   }
 }
