5ed20817593c82e489b0e17cf08024e9f133833b
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-13 01:39:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-const app = "I don't do much."
+
+
+var dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = '#FF69B4';
+
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', ' ');
+  var right = parseInt(rightNumbers, 10);
+  
+  if(right > 0) {
+    dodger.style.right = `${right - 1}px`;
+  }
+}
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', ' ');
+  var left = parseInt(leftNumbers, 10);
+  
+  if(left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+
+document.addEventListener('keydown', function(e){
+ 
+  if(e.which === 37) {
+    moveDodgerLeft();
+  }
+  
+  if(e.which === 39) {
+    moveDodgerRight();
+  }  
+ 
+});
