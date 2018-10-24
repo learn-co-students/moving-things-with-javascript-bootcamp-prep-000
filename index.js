90965350bf50eccf6dba43d761e9a8d3366f1ab4
David Choi <daaave@gmail.com>
2018-10-24 07:40:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much."
+
+function moveDodgerRight(){
+  let rightNumbers = dodger.style.right.replace("px", "");
+  let right = parseInt(rightNumbers, 10);
+  
+  if(right > 0){
+    dodger.style.right = `${right - 1}px`;
+  }
+}
