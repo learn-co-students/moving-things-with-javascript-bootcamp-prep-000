965926add1aec170ad82c5e93eb42d75fc77c206
Michael Colgan <michaelwcolgan@gmail.com>
2018-10-25 16:21:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+var dodger = document.getElementById('dodger');
+
+
+
+    document.addEventListener('keydown', function(e) {
+      if (e.which === 37) {
+        moveDodgerLeft()
+      } else if (e.which === 39) {
+        moveDodgerRight()
+      }
+    })
+
+    function moveDodgerLeft() {
+      var leftNumbers = dodger.style.left.replace('px', '')
+      var left = parseInt(leftNumbers, 10)
+
+      if (left > 0) {
+        dodger.style.left = `${left - 1}px`
+      }
+    }
+
+    function moveDodgerRight() {
+      var leftNumbers = dodger.style.left.replace('px', '')
+      var left = parseInt(leftNumbers, 10)
+
+      if (left > 0) {
+        dodger.style.left = `${left + 1}px`
+      }
+    }
