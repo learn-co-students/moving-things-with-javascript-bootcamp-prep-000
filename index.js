41f8e7612784e249c87b2129046c62acd2c3e1b4
Paul-Cl-ark <clark.paul123@gmail.com>
2018-10-27 12:45:56 +0100

    Doesn't work because JS and jQuery not installed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const app = "I don't do much."
+
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+
+    dodger.style.left = `${left - 1}px`
+  }
+})
+
+var dodger = document.getElementById('dodger')
+
+
+
+function moveDodgerLeft(dodger) {
+
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
