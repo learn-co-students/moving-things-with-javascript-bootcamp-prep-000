bc2b634efeea920b5cdbc0e727f821f568eb52f7
Brian <briandsalemi@gmail.com>
2018-11-08 19:17:52 +0000

    Done.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function moveDodgerRight() {
 var rightNumbers = dodger.style.right.replace('px','');
 var right = parseInt(rightNumbers, 10);
 if(right > 0) {
-dodger.style.right = `${right - 1}px` }}
+dodger.style.right = `${right-1}px` }}
 
 document.addEventListener('keydown', function(e) {
 if(e.which === 39) {
