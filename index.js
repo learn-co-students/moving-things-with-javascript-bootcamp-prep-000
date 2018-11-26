8bc8b99b6cf89c88513627364916d287eee961c6
Pete Hanner <petehanner@gmail.com>
2018-11-26 01:33:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+
+var dodger = document.getElementById('dodger')
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left - 3}px`
+  }
+}
+
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.left.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+
+  if (right < 360){
+    dodger.style.left = `${right + 3}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft()
+  }
+  if (e.which === 39){
+    moveDodgerRight()
+  }
+})
