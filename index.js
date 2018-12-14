ee915f8685ad8107bb6d27e44777a1e3d62e06b6
Andrei Sergo <a.sergoucht@gmail.com>
2018-12-14 17:32:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-const app = "I don't do much."
+// move square within boundaries of box
+
+var dodger = document.getElementById('dodger')
+dodger.style.backgroundColor = '#FF69B4'
+dodger.style.bottom = '0px'
+
+function moveLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+  
+  if (left > 0) {
+    dodger.style.left = `${left - 15}px`
+  }
+}
+
+function moveRight() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+  
+  if (left < 360) {
+    dodger.style.left = `${left + 15}px`
+  }
+}
+
+function moveUp() {
+  var bottomNumbers = dodger.style.bottom.replace('px', '')
+  var bottom = parseInt(bottomNumbers, 10)
+  
+  if (bottom < 360) {
+    dodger.style.bottom = `${bottom + 15}px`
+  }
+}
+
+function moveDown() {
+  var bottomNumbers = dodger.style.bottom.replace('px', '')
+  var bottom = parseInt(bottomNumbers, 10)
+  
+  if (bottom > 0) {
+    dodger.style.bottom = `${bottom - 15}px`
+  }
+}
+
+document.addEventListener('keydown', function(e){
+  if (e.which === 37) {
+    moveLeft()
+  } else if (e.which === 38) {
+    moveUp()
+  } else if (e.which === 39) {
+    moveRight()
+  } else if (e.which === 40) {
+    moveDown()
+  }
+})
+
+
