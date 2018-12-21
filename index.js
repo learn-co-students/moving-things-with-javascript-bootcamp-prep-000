439368141b3212ee28e34946af606e6fe3c8b25b
Mahbubur Rahman <mail@mahbub.me>
2018-12-21 14:27:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-const app = "I don't do much."
+var dodger = document.getElementById('dodger');
+var game = document.getElementById('game');
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  var gameWidth = game.offsetWidth;
+  var dodgerWidth = dodger.offsetWidth;
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+ 
+  if ( left < ( gameWidth-dodgerWidth ) ) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  console.log(e.which);
+  
+  if ( e.which === 37 ) {
+    moveDodgerLeft()
+  }
+  else if( e.which === 39 ) {
+    moveDodgerRight()
+  }
+})
