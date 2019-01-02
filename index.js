930e3e31b2c4e429dd071d0ce4598489128b2278
Joe Muro <jmuro0321@gmail.com>
2019-01-02 17:43:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const app = "I don't do much."
+
+function moveDodgerLeft() {
+  var dodger = document.getElementById('dodger')
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 5}px`
+  }
+}
+
+function moveDodgerRight() {
+  var dodger = document.getElementById('dodger')
+  var leftPos = dodger.style.left.replace('px', '')
+  var leftPosNum = parseInt(leftPos, 10)
+ 
+  if (leftPosNum < 360) {
+    dodger.style.left = `${leftPosNum + 5}px`
+  }
+}
