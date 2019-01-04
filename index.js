d4027a5d78c0277c46f7ede9877e84c388e75f2a
eselbeus <eselbeus@gmail.com>
2019-01-04 03:08:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much."
+// function moveDodgerRight() {
+//   var rightNumbers = dodger.style.right.replace('px', '')
+//   var right = parseInt(rightNumbers, 10)
+ 
+//   if (right < 360) {
+//     dodger.style.right = `${right - 1}px`
+//   }
+// }
