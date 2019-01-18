a6887bb9e9ddd58b59be5202d5b43c23bbc971bf
Kai Cruz <dredoreiruk@aol.com>
2019-01-18 08:16:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+var dodger = document.getElementById('dodger');
+dodger.style.backgroundColor = '#FF69B4'
+dodger.style.bottom = '0px'
+dodger.style.left = '180'
+
+function moveDadgerLeft(){
+  var leftNumbers = dodger.style.left.replace('px','')
+  var left = parseInt(leftNumbers, 10)
+  if (left>0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+function moveDodgerRight(){
+  var leftNumbers = dodger.style.left.replace('px','')
+  var left = parseInt(leftNumbers, 10)
+  if (left<360){
+    dodger.style.left = `${left +1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e){
+  if (e.which === 37){
+    moveDodgerLeft()
+  }
+  if (e.which === 39){
+    moveDodgerRight()
+  }
+})
+
