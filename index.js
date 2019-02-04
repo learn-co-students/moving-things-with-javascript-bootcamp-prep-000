e24c94ba002ace78c7589eae6b8dd50e6d7927a0
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-04 07:03:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const app = "I don't do much."
+
+function moveDodgerRight() {
+  document.addEventListener('keydown', function(e) {
+    if (e.which === 39) {
+      var leftNumbers = dodger.style.left.replace('px', '')
+      var left = parseInt(leftNumbers, 10)
+   
+      dodger.style.left = `${left + 4}px`
+    }
+  })
+}
