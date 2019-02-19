6b4756bd4313fb41c466ef8e2679f320dc0f36ae
Mary Selig <seligmar@gmail.com>
2019-02-19 19:22:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', '');
+  var right = parseInt(rightNumbers, 10);
+ 
+  if (right > 0) {
+    dodger.style.right = `${right - 1}px`; 
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 39) {
+    moveDodgerRight();
+  }
+}); 
