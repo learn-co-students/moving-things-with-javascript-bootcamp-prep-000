d0afe3ebc500ecb56b815f1d2fc12a6a733895e6
Monika Tadross <monikk.tadross@gmail.com>
2019-02-21 21:48:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-const app = "I don't do much."
+//in HTML div id="dodger" changed left to right= '180px' bc right was '0px' when we tried to move dodger to the left
+
+/*
+var dodger = document.getElementById('dodger')                  
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+ 
+  if (right > 0) {
+    dodger.style.right = `${right - 1}px`
+  }
+}
+
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 39) {console.log('click')
+    moveDodgerRight()
+  }
+})
+*/
