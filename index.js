f6d3f34e4f95a38091895d90f003aead2598f713
Will Smith <smithw1994@gmail.com>
2019-03-04 21:12:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much."
+var dodger = document.getElementById('dodger')
+
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft()
+  } else if (e.which === 39) {
+    moveDodgerRight()
+  }
+})
