cab8c34ed9359f07a446cf52004cece220089cc7
Jonny Rodriguez <jojoroto@gmail.com>
2019-03-21 13:04:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+var dodger = document.getElementById('dodger');
+var moveDoggerRight = e => {
+  if (e.which === 39) {
+    var right = parseInt(dodger.style.right.replace('px', ''), 10);
+    if (right > 0) dodger.style.right = `${right - 1}px`;
+  }
+};
+
+document.addEventListener('keydown', moveDoggerRight );
