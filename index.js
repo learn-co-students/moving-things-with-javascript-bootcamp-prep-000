6c21957431e31c54d96c816be0446cc658e3ad6c
Elaine Thai <ethai1827@gmail.com>
2019-05-17 00:11:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-const app = "I don't do much."
+var dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  var leftNums = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNums, 10)
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+function moveDodgerRight() {
+  var rightNum = dodger.style.right.replace('px', '')
+  var right = parseInt(rightNum, 10)
+  if (right > 0) {
+    dodger.style.right = `${right - 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft()
+  }
+  if (e.which === 39) {
+    moveDodgerRight()
+  }
+})
