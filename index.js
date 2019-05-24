173c2720dc13d99182855d9fc6b6d55e723ca593
Eric J Wilhite <ericjwilhite@gmail.com>
2019-05-24 04:09:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+var dodger = document.getElementById('dodger');
+document.addEventListener('keydown', function(e) {
+  if(e.which === 37) {
+    moveDodgerLeft();
+  } else if(e.which === 39) {
+    moveDodgerRight();
+  }
+});
+
+function moveDodgerLeft(e) {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+  if(left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+  if(left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
