0f939b76f81559123c529ce685b40bda7556ab64
communal-principle-3181 <aellis19@outlook.com>
2019-05-27 23:40:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const app = "I don't do much."
+
+function moveDodgerRight()
+{
+  document.addEventListner('keydown', function(e)
+  {
+    if(e.which === 39)
+    {
+      var rightNumbers = dodget.style.right.replace('px', '');
+      var right = parseInt(rightNumbers, 10);
+      dodger.style.left = `${left -1 }px`;
+    }
+  });
+}
