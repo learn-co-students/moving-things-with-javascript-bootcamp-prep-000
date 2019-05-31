b0553160b6f63a93d7dce8b3432b11fe9361ec73
Eric Aniag <ericaniag@gmail.com>
2019-05-31 03:24:44 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -16,3 +16,32 @@ describe('index', () => {
     expect(true).toBe.true
   })
 })
+
+var dodger = document.getElementById('dodger')
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+ 
+  if (right > 0) {
+    dodger.style.right = `${right - 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft()
+  }
+  if (e.which === 39) {
+    moveDodgerRight()
+  }
+})
