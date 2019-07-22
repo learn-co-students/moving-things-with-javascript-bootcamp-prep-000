48d76d7fdf4cbeac9e93a9c51681343d4a6411af
Aaron Blum <blumaa@gmail.com>
2019-07-22 23:46:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const app = "I don't do much."
+
+var dodger = document.getElementById('dodger')
+
+
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 39) {
+    moveDodgerRight()
+  }
+})
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var right = parseInt(leftNumbers, 10)
+
+  if (right < 360) {
+    dodger.style.left = `${right + 1}px`
+  }
+}
