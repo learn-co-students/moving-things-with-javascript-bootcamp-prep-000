7ea9deb3048c9f6736ddc8df8ac0aee29d4a5694
Jack OMalley <john.omalley@wework.com>
2019-09-02 18:22:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+
+var dodger = document.getElementById('dodger')
+
+document.addEventListener('keydown', function(e) {
+  console.log(e.which)
+})
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft()
+  }
+  if(e.which === 39){
+    moveDodgerRight()
+  }
+})
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+function moveDodgerRight(){
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  if (left < 400) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
