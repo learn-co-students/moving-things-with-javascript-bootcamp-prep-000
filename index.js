4ca80b59b47d63a8ad89ff3bece777f2a88fdbe7
Michael Nnaji <mnnaji3@uwo.ca>
2019-12-31 09:54:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 const app = "I don't do much."
+function moveDodgerRight(){
+  document.addEventListener('keydown', function(e) {
+  if (e.which === 39) {
+    var rightNumbers = dodger.style.right.replace('px', '')
+   var right = parseInt(rightNumbers, 10)
+
+  if (right > 0) {
+    dodger.style.right = `${left + 1}px`
+  }
+}
+})
+}
+
+function moveDodgerLeft(){
+  document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    var leftNumbers = dodger.style.left.replace('px', '')
+   var left = parseInt(leftNumbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+})
+}
