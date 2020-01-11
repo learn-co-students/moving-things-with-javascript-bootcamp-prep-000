02a802c9c60ff079de43c8d6c0d6f3ac9b34a049
Michael Nnaji <mnnaji3@uwo.ca>
2020-01-11 04:55:40 +0000

    Done.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function moveDodgerRight(){
 
 function moveDodgerLeft(){
   document.addEventListener('keydown', function(e) {
-  if (e.which === 37) {
+  if (e.which === 38) {
     var leftNumbers = dodger.style.left.replace('px', '')
    var left = parseInt(leftNumbers, 10)
 
