42d42095060591e08498e374b3d11e65442b2795
Evan Pavone <evanrpavone@gmail.com>
2020-07-26 15:43:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const app = "I don't do much."
+//39
+var dodger = document.getElementById('dodger')
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px','')
+  var left = parseInt(leftNumbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+function movedDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px','')
+  var right = parseInt(rightNumbers, 10)
+
+  if (right > 0) {
+    dodger.style.right = `${right - 1}px`
+  }
+}
