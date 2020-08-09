4bdbafb22e4a55d673fce6e7106901a5f9d7ae5b
candescent-sequence-1308 <amandaashmen@gmail.com>
2020-08-09 22:39:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much."
+
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+ 
+  if (right > 0) {
+    dodger.style.right = `${right + 1}px`
+  }
+}
