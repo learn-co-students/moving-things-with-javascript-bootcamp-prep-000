0955bed06725b9421588f595b92a04e2f89e0c3d
Robert Shilcof <robert.shilcof@gmail.com>
2020-10-08 15:38:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+
+function moveDodgerLeft(dodger) {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight(dodger) {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+ 
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
